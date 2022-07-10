facd10db9c0ccf52eea8c13c12fcf2da4e64ff91
Abhilash Raju <abhilash.raju8@gmail.com>
2022-07-10 19:33:56 +0530

    First Commit

---
+++ frontend/src/app/services/authordata.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { AuthorModel } from '../components/authors/authors.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthordataService {
+
+  constructor(private http:HttpClient) { }
+   getAuthors(){
+     return this.http.get("http://localhost:3000/authors");
+   }
+  //  newAuthor(item: AuthorModel){
+    newAuthor(formData: FormData){
+    return this.http.post("http://localhost:3000/authors/insert",formData)
+    .subscribe(data =>{console.log(data)})
+  }
+
+
+  getAuthor(id:any){
+    return this.http.get("http://localhost:3000/authors/"+id);
+  }
+
+  // delete a author
+  deleteAuthor(id:any)
+  {
+console.log("id to delete", id);
+    return this.http.delete("http://localhost:3000/authors/remove/"+id)
+
+  }
+  //update a author
+  editAuthor(author:any)
+  {
+    console.log('Author update')
+    return this.http.put("http://localhost:3000/authors/update",author)
+    .subscribe(data =>{console.log(data)})
+  }
+}
---
+++ frontend/src/app/services/bookdata.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import {BooksModel} from '../components/books/books.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookdataService {
+
+  constructor(private http:HttpClient) { }
+  getBook(id:any){
+    return this.http.get("http://localhost:3000/books/"+id);
+  }
+
+   getBooks(){
+     return this.http.get("http://localhost:3000/books");
+   }
+    newBook(formData: FormData){
+    return this.http.post("http://localhost:3000/books/insert",formData)
+    .subscribe(data =>{console.log(data)})
+  }
+  // delete a book
+  deleteBook(id:any)
+  {
+console.log("id to delete", id);
+    return this.http.delete("http://localhost:3000/books/remove/"+id)
+
+  }
+  //update a book
+  editBook(book:any)
+  {
+    console.log('book update')
+    return this.http.put("http://localhost:3000/books/update",book)
+    .subscribe(data =>{console.log(data)})
+  }
+}
